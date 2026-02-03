a728abe142bbcc946240c45fad5fc43d70fd6af0
jknotjoking <lance.huang@valour.com.tw>
2026-02-03 23:22:24 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,29 +3,29 @@ const rawGuestList = `
 0	Groom Jack、Bride Kensey、主婚人 蔡寬福、主婚人 張瑢糴、主婚人 徐瑜樺、主婚人 李武男、阿嬤 侯玉珠、大伯父 蔡松柏、大伯母 蔡林金蓮、大舅 徐崇偉、小舅 徐晟偉、小舅媽 郭憓陵、舅舅 張志強、舅媽 陳亮今
 1	蔡佳雯、陳品全、陳宥儒、陳羿璇、蔡英哲、蔡沛霈、蔡佳秋、陳順昌、陳佩妤、陳楷元、林依妮、蔡沅恩
 2	PP Kevin 陳怡全、PE Paper 陳富瑞、PP Climber 陳睦彥、張月華、PP Solomon 陳文慶、余麗霞、PP Stephen 曾大中、蔣以文、Benz 邱禎雄、陳麗珠
-3	CP Richard 陳國雄、PP Amanada 邱南嫣、PP Gino 林明聰、廖淑美、P IQ 胡儷馨、PE Alan 林伯倫、James Huang 黃丁士、周祝如、陳淑靜、Ken Yuan
-4	IPP David、Jane、Tanya、Michelle、Lily、Yvette、Jeanette、Grace、林家妃、Sophie 翁淑惠
-5	連文彬、許文泉、謝宜良、王政琦、林怡伶 Ivy、郭思寬、曹心寗、曹柇軒、Woody、Sammi、曹人宇、洪雪裕
-6	鄭立仁 Jack、廖慧伶、謝涴亦 Gina、高紹瑀 Amber、高菁蔚 Vivian、曾靜怡Yudy、徐家清 Sissi、廖玉美、蔣可怡 Teri、安東尼、吳婕柔 Winnie
-7	蔡孟攸、St Cloud、張馥淇、張維恩、張瑜庭、Ivy Dodo、易慎慈、張雅憶、林霆宥、張塋鈺、林穎菲
-8	陳解文、劉豐逸、蔡妤萱、蔡閔森、汪嘉慧、陳怡媚、陳冠佑、鄭言晞、鄭芯宥、蔡昀頤、蔡溱羽、蔡溱秝
-9	李嘉芬、陳永正、大野友義、Ono Yoshie、蔡筠、李孟玲 Linda、王家玉、黃大倫、鍾志、連素哲
+3	P IQ 胡儷馨、PE Alan 林伯倫、James Huang 黃丁士、周祝如、陳淑靜、Ken Yuan、林怡伶 Ivy、郭思寬、江俊豪、黃錦雯
+4	IPP David、Jane、Tanya、Michelle、Lily、Yvette、Jeanette、Grace、陳怡靜、楊麗桂
+5	連文彬、許文泉、謝宜良、王政琦、曹心寗、曹柇軒、Woody、Sammi、曹人宇、洪雪裕、Sophie 翁淑惠
+6	謝涴亦 Gina、高紹瑀 Amber、高菁蔚 Vivian、曾靜怡Yudy、徐家清 Sissi、廖玉美、蔣可怡 Teri、安東尼、吳婕柔 Winnie、謝義宏
+7	蔡孟攸、St Cloud、張馥淇、張瑜庭、Ivy Dodo、易慎慈、張雅憶、林霆宥、張塋鈺、林穎菲
+8	陳解文、劉豐逸、蔡妤萱、蔡閔森、汪嘉慧、陳怡媚、陳冠佑、蔡昀頤、蔡溱羽、蔡溱秝
+9	李嘉芬、林家妃、大野友義、Ono Yoshie、蔡筠、李孟玲 Linda、王家玉、黃大倫、鍾明孝、連素哲
 10	馬靜如 PDG Sara、Ralph、莊金英 PP Linda、陳耀祖 James、秦嗣林 PP Stone、徐靜孏、彭達賢 PP Gear、許靜慧、吳榮發 PP Donny、林芷亦 PP Jill
-11	PP Amanada、PP Linda、PP Shain、PP Keane、PP Bear、PP Franey、PP Godspeed、PP Eric 王金祥、PP Frank、Leona
+11	PP Amanda、PP Linda、PP Shain、PP Keane、PP Bear、PP Franey、PP Godspeed、PP Eric 王金祥、PP Frank、Leona
 12	張乃聖、李智惠、翁世慧、曾新衡、韓曉君、張軒瑜、楊惠蘭、曾慶星、許明文、張毓婷
-13	二伯父 蔡松山、二伯母 蔡陳對、三伯父 蔡松義、三伯母 陳玉珠、姑姑 蔡沂澐、姑丈 陳振成、堂哥 蔡志忠、堂哥 蔡哲賢、大嫂 陳晏真、堂姐 蔡靜宜
+13	二伯父 蔡松山、二伯母 蔡陳對、三伯父 蔡松義、三伯母 陳玉珠、姑姑 蔡沂澐、姑丈 陳振成、堂哥 蔡志忠、堂哥 蔡哲賢、大嫂 陳晏真、堂姐 蔡靜宜、鄭言晞、鄭芯宥
 14	五星好評 瑞雲姐、姨丈 張人文、雅雅阿姨、莊壹証、舅公 侯德宗、Ray、洪美雅、林于倫、小舅 侯逯陞 Cary、小舅媽 Nicole、舅婆、Ivy
 15	叔公 張朝國、姨婆 張楊寶蓮、哥哥 張瑞堂、舅公 楊得元、伯父 何志傑、漂亮姨婆 王春梅、叔公 李武龍、大伯父 李文章、叔叔 洪聞、洪聞夫人 王秋萍
 16	Fanny Chang、Kevin Liu、Jessie Chang、Brian Yeh、Joanna、James Lee、Emily Chiu、Eason Hsu、Hsin Pei Chou、軒哥阿嬤、吳小珈
-17	Maggie Tang、Capybara Otani、Catherine Hsiao、Oscar Tang、Sisa Chiang、Ivan Ivanov、Alexandar Ivanov、Ghini Iong、Lorrain Sun、Avery Chiang
+17	Maggie Tang、Capybara Otani、Catherine Hsiao、Oscar Tang、Sisa Chiang、Ivan Ivanov、Alexandar Ivanov、Ghini Iong、Lorrain Sun、Avery Chiang、Lucas Hung、Louise Ho
 18	Steve Tang、Anderson Goh、Tiffany Lee、Yaya Lau、Justin Hui、Amber Chiu、Angeline Kuo、Chunkiat Ong、Wenxin Chiam、Jill Lin、Amy Williams
-19	John、Eric、950、Mag、Hans、Weber、Joseph、Jay、Vivian、李家碩
-20	林佳欣、黃其安、江明勳 莫凝123、洪邵澤 ccs、喜德、Rax、樹哥、小米、Bala、志祥
+19	John、Eric、950、Mag、Hans、Weber、Joseph、Jay、Vivian、李家碩、Allison、Steven
+20	林佳欣、黃其安、江明勳 莫凝123、洪邵澤 ccs、喜德、Rax、樹哥、小米、Bala、志祥、Eric Hu
 21  Uncle 李昱頡、劉福財、劉福財夫人、教授 何四郎、伯父 林文欽、張仁德、張仁德夫人、李守益、洪名義、曾昭雄
 22  李佳妙、吳英立、李怡儒、蔡蕙霞、二姑婆 徐綉玲、二姑丈 李專榮、徐千純、謝元峰、謝承儒、謝承哲
 23  Cherry Yang、Gerry Lin、Annie Lin、Bryan Lin、語柔、Yan Lin、Cindy、Miho Lin、Kimi Lin、冠廷、永婕
-24  Candice Chia、Kevin Chen、Lucas Hung、Louise Ho、Hali Liu、Lance Huang、Irene、丸少爺、Emma Fang、黃子耘、吳佳旻
-25  Michael、Belle、Webber、Vera、Megan、Nikki、David、Allison、Steven、Zoe
+24  Candice Chia、Kevin Chen、Hali Liu、Lance Huang、Emma Fang
+25  Michael、Belle、Webber、Vera、Megan、Nikki、David、Zoe、Irene、丸少爺、黃子耘、吳佳旻
 26  
 `;
 
